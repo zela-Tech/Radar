856a08e7bc35babda743cc5c1688d83bfb8dd8b4
Adom Z <zelalemadom@gmail.com>
2026-03-24 00:10:10 -0400

    feat: add basic onboarding flow with completion handler

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import '../helper/user_session_helper.dart';
+import './main_navigator.dart';
 
 class OnboardingScreen extends StatelessWidget {
   final int userId;
@@ -9,7 +10,18 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Onboarding for user $userId')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await SessionHelper.setOnboardingDone();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (_) => const MainNav()),
+            );
+          },
+          child: const Text("Finish Onboarding"),
+        ),
+      ),
     );
   }
 }
